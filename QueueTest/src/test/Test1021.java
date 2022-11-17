1e689a2fa7afe58feae74572299ba4c1cf9ccd72
jh kim <pjhkim0408@gmail.com>
2022-11-17 22:01:09 +0900

    test1021

--- QueueTest/src/test/Test1021.java
+++ QueueTest/src/test/Test1021.java
@@ -27,31 +27,40 @@ public class Test1021 {
 			takeOut[i] = sc.nextInt();
 		}
 		
+		
 		for(int i = 0; i < M; i++) {
-			int target_idx = deque.indexOf(takeOut[i]);
-			int half_idx;
+			int target_idx = deque.indexOf(takeOut[i]); // 추출할 수의 인덱스
+			int half_idx; // 중앙의 인덱스
 			
-			if(deque.size() % 2 == 0) {
+			// {1,2,3,4} --> half_idx == 1
+			// {1,2,3,4,5} --> half_idx == 2
+			if(deque.size() % 2 == 0) { // 크기가 짝수일 때
 				half_idx = deque.size() / 2 - 1;
 			}
-			else {
+			else { // 크기가 홀수일 때
 				half_idx = deque.size() / 2;
 			}
-			
+			// 2번 연산
+			// 추출할 수가 중앙값보다 앞에 있으면
 			if(target_idx <= half_idx) {
 				for(int j = 0; j < target_idx; j++) {
+					// 추출할 수 직전까지를 모두 뒤로 이동
 					int tmp = deque.pollFirst();
 					deque.offerLast(tmp);
 					count++;
 				}
 			}
+			// 3번 연산
+			// 추출할 수가 중앙값보다 뒤에 있으면
 			else {
 				for(int j = 0; j < deque.size() - target_idx; j++) {
+					// 추출할 수까지 앞으로 이동
 					int tmp = deque.pollLast();
 					deque.offerFirst(tmp);
 					count++;
 				}
 			}
+			// 추출
 			deque.pollFirst();
 		}
 		System.out.println(count);
